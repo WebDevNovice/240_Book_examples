977e6707bfd1ec1f353e046ccaf3677c88930d23
WebDevNovice <jacobgbullock3@gmail.com>
2024-06-26 12:24:35 -0600

    This is the book example code

---
+++ ch01/sec01/practiceInputOutput.java
@@ -0,0 +1,10 @@
+package ch01.sec01;
+
+import java.util.Scanner;
+
+public class practiceInputOutput {
+    public static void main(String[] args){
+        System.out.println("What is your name?\n");
+        Scanner name = new Scanner(System.in);
+    }
+}
